b3cf98135d6de4b0209a1b0425351f4b8a1cf8d9
bluewon <quantranesc@gmail.com>
2021-05-06 01:21:11 +0700

    Hmac cookie example

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module github.com/bluewon/go-arch
 
-go 1.15
+go 1.16
 
-require golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- main.go
+++ main.go
@@ -1,38 +1,80 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
 	"fmt"
-	"log"
-
-	"golang.org/x/crypto/bcrypt"
+	"io"
+	"net/http"
+	"strings"
 )
 
 func main() {
-
-	pass := "123456789"
-	hashedPass, err := hashPassword(pass)
-	if err != nil {
-		panic(err)
-	}
-	err = comparePassword(pass, hashedPass)
-	if err != nil {
-		log.Fatalln("Not logged in")
-	}
-	log.Println("Logged in!")
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/submit", bar)
+	http.ListenAndServe(":8080", nil)
 }
 
-func hashPassword(password string) ([]byte, error) {
-	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return nil, fmt.Errorf("error while generating bcrypt hash from password : %w", err)
-	}
-	return bs, nil
+func getCode(msg string) string {
+	h := hmac.New(sha256.New, []byte("I love tuesday when I meet her"))
+	h.Write([]byte(msg))
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
 
-func comparePassword(password string, hassedPass []byte) error {
-	err := bcrypt.CompareHashAndPassword(hassedPass, []byte(password))
-	if err != nil {
-		return fmt.Errorf("Invalid password: %w", err)
+func bar(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
 	}
-	return nil
+	email := r.FormValue("email")
+	if email == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+	code := getCode(email)
+	//hash / message digest / digest / hash value | what we stored
+	c := http.Cookie{
+		Name:  "session",
+		Value: code + "|" + email,
+	}
+	http.SetCookie(w, &c)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("session")
+	if err != nil {
+		c = &http.Cookie{}
+	}
+	xs := strings.SplitN(c.Value, "|", 2)
+	isEqual := true
+	if len(xs) == 2 {
+		cCode := xs[0]
+		cEmail := xs[1]
+		code := getCode(cEmail)
+		isEqual = hmac.Equal([]byte(cCode), []byte(code))
+	}
+	message := "Not logged in"
+	if isEqual {
+		message = "Logged in"
+	}
+	html := `<!DOCTYPE html>
+	 <html lang ="en">
+	 <head>
+	 	<meta charset="UTF-8">
+		<meta name="author" content="Shoun Ouan">
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	 	<title>Hmac example</title>
+	 </head>
+	 <body>
+	 	<p> Cookie value: ` + c.Value + `<p/>
+		 <p> ` + message + `<p/>
+		<form action="/submit" method="POST">
+			<input type="email" name="email" />
+			<input type="submit" />
+		</form>
+	 </body>
+	 </html>`
+	io.WriteString(w, html)
 }
